ca1565c06534f39486e892a4cbef8b7c0f264198
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2024-08-13 11:56:47 -0300

    update

---
+++ default-package/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Conta cc = new ContaCorrente(500.00);
+        Conta cp = new Contapopanca(100.00);
+
+        cc.depositar(150.00);
+        cc.sacar(50.00);
+        cc.transferir(200.00, conta2);
+
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
+    }
+}
